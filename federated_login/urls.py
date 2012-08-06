973049d1555f7d39b668c7b2c3a242388f3956f7
Anna Warzecha <anna.warzecha@gmail.com>
2012-08-06 09:54:31 +0200

    Use settings LOGIN_URL

--- federated_login/urls.py
+++ federated_login/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import patterns, url
-
+from django.conf import settings
 
 urlpatterns = patterns('federated_login.views',
-    url(r'^sign-in/$', 'login', name='fl_login'),
+    url(r'^%s$' % settings.LOGIN_URL.lstrip('/'), 'login', name='fl_login'),
     url(r'^identity/$', 'identity', name='fl_identity')
 )
