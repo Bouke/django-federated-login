3f8d3ae7ed90937dd82230ea23d1ac8e96c5f681
Bouke Haarsma <bouke@webatoom.nl>
2012-08-26 14:10:31 +0200

    Pushed version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django-federated-login',
-    version='0.1.4',
+    version='0.2.0',
     author='Bouke Haarsma',
     author_email='bouke@webatoom.nl',
     packages=[
